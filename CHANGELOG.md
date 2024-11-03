5f4928f5d6d603ed5fdeaa27cb296e39cd04d728
Langning Chen <chenlangning2009@outlook.com>
2024-11-03 17:00:52 +0800

    Fix the issue of displaying the relative time

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,9 +2,8 @@
 
 All notable changes to the "cyezoi" extension will be documented in this file.
 
-## v0.0.16
+## v0.0.17
 
-- Emergency fix for the scoreboard
-- Remove previous log
+- Fix the issue of displaying the relative time
 
 Want to see the previous log? [Click here](https://github.com/CYEZOI/cyezoi-helper/commits/main/CHANGELOG.md)
--- src/utils.ts
+++ src/utils.ts
@@ -2,22 +2,22 @@ import * as vscode from 'vscode';
 import { outputChannel } from "./io";
 
 export const getUnit = (data: number, unit: string) => {
-    if (data >= 1 && data < 2) { return data + ' ' + unit; }
-    else { return data + ' ' + unit + 's'; }
+    if (data >= 1 && data < 2) { return unit; }
+    else { return unit + 's'; }
 };
 export const toTime = (time: number): string => {
-    if (time < 1000) { return time.toFixed(0) + 'ms'; } time /= 1000;
-    if (time < 60) { return time.toFixed(2) + 's'; } time /= 60;
-    if (time < 60) { return time.toFixed(2) + getUnit(time, 'minute'); } time /= 60;
-    if (time < 24) { return time.toFixed(2) + getUnit(time, 'hour'); } time /= 24;
-    if (time < 30) { return time.toFixed(2) + getUnit(time, 'day'); } time /= 30;
-    if (time < 12) { return 'about ' + Math.floor(time) + getUnit(time, 'month'); } time /= 12;
-    return 'about ' + Math.floor(time) + getUnit(time, 'year');
+    if (time < 1000) { return time + ' ms'; } time = Math.floor(time / 1000);
+    if (time < 60) { return time + ' s'; } time = Math.floor(time / 60);
+    if (time < 60) { return time + ' ' + getUnit(time, 'minute'); } time = Math.floor(time / 60);
+    if (time < 24) { return time + ' ' + getUnit(time, 'hour'); } time = Math.floor(time / 24);
+    if (time < 30) { return time + ' ' + getUnit(time, 'day'); } time = Math.floor(time / 30);
+    if (time < 12) { return 'about ' + time + ' ' + getUnit(time, 'month'); } time = Math.floor(time / 12);
+    return 'about ' + time + ' ' + getUnit(time, 'year');
 };
 export const toMemory = (time: number): string => {
-    if (time < 1024) { return time.toFixed(0) + 'B'; } time /= 1024;
-    if (time < 1024) { return time.toFixed(2) + 'KiB'; } time /= 1024;
-    if (time < 1024) { return time.toFixed(2) + 'MiB'; } time /= 1024;
+    if (time < 1024) { return time + 'B'; } time = Math.floor(time / 1024);
+    if (time < 1024) { return time + 'KiB'; } time = Math.floor(time / 1024);
+    if (time < 1024) { return time + 'MiB'; } time = Math.floor(time / 1024);
     return time.toFixed(2) + 'GiB';
 };
 export const toRelativeTime = (time: number): string => {
@@ -25,11 +25,11 @@ export const toRelativeTime = (time: number): string => {
     const suffix = (time > now ? 'later' : 'ago');
     var delta = Math.floor(Math.abs(now - time) / 1000);
     if (delta < 60) { return 'just now'; } delta = Math.floor(delta / 60);
-    if (delta < 60) { return delta + getUnit(delta, 'minute') + ' ' + suffix; } delta = Math.floor(delta / 60);
-    if (delta < 24) { return delta + getUnit(delta, 'hour') + ' ' + suffix; } delta = Math.floor(delta / 24);
-    if (delta < 30) { return delta + getUnit(delta, 'day') + ' ' + suffix; } delta = Math.floor(delta / 30);
-    if (delta < 12) { return 'about ' + delta + getUnit(delta, 'month') + ' ' + suffix; } delta = Math.floor(delta / 12);
-    return 'about ' + delta + getUnit(delta, 'year') + ' ' + suffix;
+    if (delta < 60) { return delta + ' ' + getUnit(delta, 'minute') + ' ' + suffix; } delta = Math.floor(delta / 60);
+    if (delta < 24) { return delta + ' ' + getUnit(delta, 'hour') + ' ' + suffix; } delta = Math.floor(delta / 24);
+    if (delta < 30) { return delta + ' ' + getUnit(delta, 'day') + ' ' + suffix; } delta = Math.floor(delta / 30);
+    if (delta < 12) { return 'about ' + delta + ' ' + getUnit(delta, 'month') + ' ' + suffix; } delta = Math.floor(delta / 12);
+    return 'about ' + delta + ' ' + getUnit(delta, 'year') + ' ' + suffix;
 };
 export const getCookiesValue = async (): Promise<string> => {
     outputChannel.trace('[utils   ]', '"getCookiesValue"');
