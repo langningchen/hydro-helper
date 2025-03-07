1fac1e51db79b85765a8d0d758d4b4199c4e1260
Langning Chen <chenlangning2009@outlook.com>
2025-03-07 17:16:16 +0800

    Add Clippy language support and refactor variable declaration in attribute parsing

--- src/attr.ts
+++ src/attr.ts
@@ -14,7 +14,7 @@ export default class {
             return new TextDecoder().decode(data);
         });
         const regex = /@(\S+)\s+(\S+)/g;
-        let match;
+        var match;
         while ((match = regex.exec(this.content)) !== null) {
             this.attributes.set(match[1], match[2]);
         }
