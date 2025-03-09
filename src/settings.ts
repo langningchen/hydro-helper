6b1d0f285df79e736002f8c203f0c36634d79b25
Langning Chen <chenlangning2009@outlook.com>
2025-03-09 12:14:06 +0800

    Refactor configuration keys to use 'hydro' namespace for server settings

--- src/settings.ts
+++ src/settings.ts
@@ -4,18 +4,18 @@ import { outputChannel } from './io';
 export default class {
     static get server(): string {
         outputChannel.trace('[settings]', '"get server.server"');
-        return vscode.workspace.getConfiguration('server').get('server') as string;
+        return vscode.workspace.getConfiguration('hydro').get('server.server') as string;
     }
     static get safeProtocol(): boolean {
         outputChannel.trace('[settings]', '"get server.safeProtocol"');
-        return vscode.workspace.getConfiguration('server').get('safeProtocol') as boolean;
+        return vscode.workspace.getConfiguration('hydro').get('server.safeProtocol') as boolean;
     }
     static get domain(): string {
         outputChannel.trace('[settings]', '"get server.domain"');
-        return vscode.workspace.getConfiguration('server').get('domain') as string;
+        return vscode.workspace.getConfiguration('hydro').get('server.domain') as string;
     }
     static async setDomain(value: string) {
         outputChannel.trace('[settings]', '"set server.domain"', arguments);
-        await vscode.workspace.getConfiguration('server').update('domain', value.toString());
+        await vscode.workspace.getConfiguration('hydro').update('server.domain', value.toString());
     }
 }
